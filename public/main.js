dea5203e9d96997d5d3edb07f2ea0356ee62801c
chadrackitsia <itsia001@gmail.com>
2020-04-04 12:25:46 +0200

    Enable pusher logging

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("vote-form");
 
+// Evenement lié à l'envoi du formulaire
 form.addEventListener("submit", (e) => {
 
   const choice = document.querySelector("input[name=candidat]:checked").value;
@@ -13,8 +14,49 @@ form.addEventListener("submit", (e) => {
     })
   })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data)) // 
     .catch(err => console.log(err))
 
   e.preventDefault();
 });
+
+
+let dataPoints = [
+  { label: 'Fayulu', y: 0 },
+  { label: 'Tshisekedi', y: 0 },
+  { label: 'Shadary', y: 0 },
+  { label: 'Kamerhe', y: 0 }
+];
+
+const chartContainer = document.querySelector('#chartContainer');
+
+if (chartContainer) {
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled : true,
+    theme : 'theme1',
+    title : {
+      text: 'Resultats des Candidats'
+    },
+    data : [
+      {
+        type : 'column',
+        dataPoints : dataPoints
+      }
+    ]
+  });
+
+  chart.render();
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher('0e5dedc09bfbf94b0788', {
+    cluster: 'eu',
+    forceTLS: true
+  });
+
+  var channel = pusher.subscribe('candidat-poll');
+  channel.bind('candidat-vote', function(data) {
+    
+  });
+}
