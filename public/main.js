d71126dfc881bf33244e05fce17cadcf44283f42
chadrackitsia <itsia001@gmail.com>
2020-04-04 19:59:22 +0200

    Main app config

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+// DOM - Selection du formulaire de votre
 const form = document.getElementById("vote-form");
 
 // Evenement lié à l'envoi du formulaire
@@ -20,43 +21,61 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+fetch('http://localhost:127/poll')
+  .then(res => res.json())
+  .then(data => {
+    const votes = data.votes;
+    var totalVotes = votes.lenght;
+    // Comptage des votes - utilisation de REDUCE (acc/current)
+    const voteCounts = votes.reduce((acc, vote) => (acc[vote.candidat] = ((acc[vote.candidat] || 0) + parseInt(vote.points)), acc), {});
 
-let dataPoints = [
-  { label: 'Fayulu', y: 0 },
-  { label: 'Tshisekedi', y: 0 },
-  { label: 'Shadary', y: 0 },
-  { label: 'Kamerhe', y: 0 }
-];
 
-const chartContainer = document.querySelector('#chartContainer');
-
-if (chartContainer) {
-  const chart = new CanvasJS.Chart('chartContainer', {
-    animationEnabled : true,
-    theme : 'theme1',
-    title : {
-      text: 'Resultats des Candidats'
-    },
-    data : [
-      {
-        type : 'column',
-        dataPoints : dataPoints
-      }
-    ]
-  });
-
-  chart.render();
-
-  // Enable pusher logging - don't include this in production
-  Pusher.logToConsole = true;
-
-  var pusher = new Pusher('0e5dedc09bfbf94b0788', {
-    cluster: 'eu',
-    forceTLS: true
-  });
-
-  var channel = pusher.subscribe('candidat-poll');
-  channel.bind('candidat-vote', function(data) {
+    let dataPoints = [
+      { label: 'Fayulu', y: voteCounts.Fayulu },
+      { label: 'Tshisekedi', y: voteCounts.Tshisekedi },
+      { label: 'Shadary', y: voteCounts.Shadary },
+      { label: 'Kamerhe', y: voteCounts.Kamerhe }
+    ];
     
+    const chartContainer = document.querySelector('#chartContainer');
+    
+    if (chartContainer) {
+      const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled : true,
+        theme : 'theme1',
+        title : {
+          text: 'Total votes ' + totalVotes
+        },
+        data : [
+          {
+            type : 'column',
+            dataPoints : dataPoints
+          }
+        ]
+      });
+    
+      chart.render();
+    
+      // Enable pusher logging - don't include this in production
+      Pusher.logToConsole = true;
+    
+      var pusher = new Pusher('0e5dedc09bfbf94b0788', {
+        cluster: 'eu',
+        forceTLS: true
+      });
+    
+      var channel = pusher.subscribe('candidat-poll');
+      channel.bind('candidat-vote', function(data) {
+        dataPoints = dataPoints.map(x => {
+          if(x.label == data.candidat){
+            x.y += data.points;
+            return x;
+          } else {
+            return x;
+          }
+        });
+        chart.render();
+      });
+    }
+
   });
-}
